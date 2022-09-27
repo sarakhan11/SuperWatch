a714ece07642dc7954507c24a831758cf2abdd97
sarakhan11 <sarakhan11@gmail.com>
2022-09-27 08:23:52 -0700

    commit2

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ import React from 'react';const Index = () => {
               </li>
             </ul>
 
-            <div className='pl-20 inset-0'>
+            <div className='pl-20 inset-0 hidden xl:block lg:block'>
               <Image className="cursor-pointer" src="/Group 303.png" width={515} height={530}/>
             </div>
             </div>
@@ -115,11 +115,11 @@ import React from 'react';const Index = () => {
 
     
 
-    <div class="pt-24 pl-20">
+    <div class="pt-24 pl-20 mx-auto">
           <h1 class="font-semibold text-6xl pb-6 text-slate-800 max-w-[624px]">
             Super Luxury Smart Watch
           </h1>
-          <p class="pb-8 text-slate-600 max-w-[405px]">
+          <p class="pb-8 text-slate-600 xl:max-w-[405px] lg:max-w-[405px] md:max-w-[505px] xl:max-w-[505px] ">
             Lorem Ipsum dolor sit amet, consectetur adipisicing elit. 
             plac erat vivamus tortor, vitae aliquam ullamcorper
           </p>
@@ -129,21 +129,21 @@ import React from 'react';const Index = () => {
         </div>
 
 
-        <div class="py-12 mt-72 container mx-auto flex">
-          <div class="w-1/2 pr-8">
-            <p class="font-medium text-4xl text-slate-800 pb-4 ">
+        <div class="mt-72 mx-20 pr-20 container mx-auto flex flex-col xl:flex-row lg:flex-row mx-auto md:flex-col sm:flex-col">
+          <div class="xl:w-1/2 lg:w-1/2 md:w-4/4 sm:w-11/12 pr-8 mx-auto">
+            <p class="font-medium text-4xl text-slate-800 pb-4 mx-auto">
               About Us
             </p>
             <p class="text-slate-600 pb-12">
               Putting a watch on your wrist has a larger impact than a simple fashion accessory. 
               The world sees the timepiece that you choose to wear so make sure it represents you.
             </p>
-            <div class="flex ">
-              <div class="flex flex-col justify-center text-center pr-12">
+            <div class="xl:flex-row lg:flex-row md:flex-col sm:flex-col flex flex-col gap-x-8">
+              <div class="flex flex-col justify-center text-center">
                 <p class="text-xl font-medium text-slate-800 pb-4">1000+</p>
                 <p class="text-slate-600">Brands</p>
               </div>
-              <div class="flex flex-col justify-center text-center pr-8">
+              <div class="flex flex-col justify-center text-center">
                 <p class="text-xl font-medium text-slate-800 pb-4 ">250k+</p>
                 <p class="text-slate-600">customer</p>
               </div>
@@ -154,82 +154,104 @@ import React from 'react';const Index = () => {
             </div>
           </div>
         
-        <div class="grid grid-cols-1 gap-y-8  w-1/2">
-          <div class=" gap-x-8 grid-cols-2 grid ">
-            <div className='gap-y-8 grid grid-cols-1'>
-              <Image className="cursor-pointer" src="/Rectangle 83.png" width={296} height={168}/>
-              <Image className="cursor-pointer" src="/Rectangle 84.png" width={296} height={168}/>
-            </div>
-            <div>
-              <Image className="cursor-pointer" src="/Rectangle 82.png" width={296} height={368}/>
+          <div class="xl:w-1/2 lg:w-1/2 md:w-4/5 sm:w-5/6 mx-auto ">
+            <div class=" gap-x-4 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 grid ">
+              <div className='gap-y-4 grid grid-cols-1 w-full'>
+                <Image className="cursor-pointer" src="/Rectangle 83.png" width={296} height={168}/>
+                <Image className="cursor-pointer" src="/Rectangle 84.png" width={296} height={168}/>
+              </div>
+              <div className='gap-y-4 grid grid-cols-1 w-full pt-0 xl:pt-0 lg:pt-0 md:pt-4 sm:pt-4'>
+                <Image className="cursor-pointer" src="/Rectangle 82.png" width={296} height={368}/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
 
       <div class="container mx-auto mt-32 mb-32 ">
-        <h1 class="text-4xl text-gray-800 font-semibold ml-48 pl-8 2xl:ml-48 2xl:pl-8 xl:ml-16 xl:pl-8 lg:ml-0 lg:pl-0">
+        <h1 class="text-4xl text-gray-800 font-semibold mx-auto pl-8 2xl:ml-48 2xl:pl-8 xl:ml-16 xl:pl-8 lg:ml-0 lg:pl-0 xl:mx-0 lg:mx-0 md:mx-auto sm:mx-auto">
           Available Products
         </h1>
-        <div className='grid grid-cols-2 '>
-        <div className='grid grid-cols-1 gap-y-8'>
-
-        <div class="mt-12 gap-12 justify-center items-center">
-          <div class=" ">
-            <div class="relative ">
-            <Image className="cursor-pointer" src="/Rectangle 78.png" width={515} height={560}/>
-              <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
-                X Hybrid Smartwatch
+        <div className=' xl:ml-0 lg:ml-0 md:ml-20 sm:ml-20 grid xl:grid-cols-2 lg:grid-cols-2 grid-cols-1 mx-auto xl:p-48 lg:p-24 md:p-24 sm:p-24 p-4 lg:gap-8'>
+          <div className='grid grid-cols-1 gap-y-8 mx-auto'>
+            <div class="mt-12 justify-center items-center">
+              <div class=" ">
+                <div class="relative ">
+                  <Image className="cursor-pointer" src="/Rectangle 78.png" width={515} height={560}/>
+                <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
+                    X Hybrid Smartwatch
+                  </div>
+                </div>
               </div>
+            </div>
+            <div class="">
+              <div class="">
+                <div class="relative">
+                <Image className="cursor-pointer" src="/Rectangle 81.png" width={515} height={330}/>
+                  <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
+                    Timex Analog Women's watch
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            </div>
+
+          <div className='grid grid-cols-1 gap-y-8 mx-auto'>
+            <div class="mt-12 justify-center items-center">
+              <div class="">
+                <div class="relative">
+                <Image className="cursor-pointer" src="/Rectangle 81.png" width={515} height={330}/>
+                  <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
+                    Timex Analog Women's watch
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="">
+              <div class=" ">
+                <div class="relative ">
+                <Image className="cursor-pointer" src="/Rectangle 78.png" width={515} height={560}/>
+                  <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
+                    X Hybrid Smartwatch
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            </div>
+            </div>
+          </div>
+
+
+          <div class="container mx-auto mb-32">
+            <div class="flex flex-col xl:flex-row lg:flex-row md:flex-col sm:flex-col">
+              <div class="w-11/12 mx-auto xl:mx-auto lg:mx-auto md:mx-36 sm:mx-24 mx-20 mb-8">
+                <h1 class="text-gray-800 font-semibold w-10/12 lg:w-11/12 xl:w-11/12 2xl:w-10/12 text-5xl mt-16">
+                  Subscribe Us And Get Upto 30% Off
+                </h1>
+                <h1 class="text-gray-600 text-xl mt-8 w-9/12 lg:w-11/12 xl:w-11/12 2xl:w-9/12">
+                  Putting a watch on your wrist has a larger impact than a simple fashion accessory
+                </h1>
+                <div class="flex gap-5 mt-12"><div class="w-6/12">
+                  <label class="relative block">
+                    <span class="sr-only">Search</span>
+                    <span class="absolute inset-y-0 left-0 flex items-center "></span>
+                    <input class="border placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-3 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Enter Your Email" type="text" name="search"/>
+                  </label>
+                </div>
+                <div class="w-5/12">
+                  <button class="px-6 py-3 bg-gray-800 text-white hover:bg-gray-900 rounded-lg">
+                    Submit Now
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="w-11/12 justify-end items-end flex mx-14 xl:mx-0 lg:mx-0 md:mx-10 sm:mx-20">
+              <Image className="cursor-pointer" src="/Rectangle 89.png" width={624} height={410}/>
             </div>
           </div>
         </div>
-
-        <div class="">
-          <div class="">
-            <div class="relative">
-            <Image className="cursor-pointer" src="/Rectangle 81.png" width={515} height={330}/>
-              <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
-                Timex Analog Women's watch
-              </div>
-            </div>
-          </div>
-        </div>
-
-        </div>
-
-        <div className='grid grid-cols-1 gap-y-8'>
-
-
-        <div class="">
-          <div class="">
-            <div class="relative">
-            <Image className="cursor-pointer" src="/Rectangle 81.png" width={515} height={330}/>
-              <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
-                Timex Analog Women's watch
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="mt-12 gap-12 justify-center items-center">
-          <div class=" ">
-            <div class="relative ">
-            <Image className="cursor-pointer" src="/Rectangle 78.png" width={515} height={560}/>
-              <div class="absolute bottom-10 left-7 bg-gray-100 py-3 px-4 rounded-lg opacity-75">
-                X Hybrid Smartwatch
-              </div>
-            </div>
-          </div>
-        </div>
-
-        </div>
-        </div>
-      </div>
-
-    
-
 
 
       </>
